be4bfe42c5f38ef4e13292e16ae36a583ccf67c2
CalebBrett <43529463+CalebBrett@users.noreply.github.com>
2019-05-20 20:34:34 -0400

    Updates

---
+++ precache-manifest.d9ddde8e1f40947b636533b054a91b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18045737f644871d04ef",
+    "url": "/static/js/main.18045737.chunk.js"
+  },
+  {
+    "revision": "bfd23eddaf123502bdcc",
+    "url": "/static/js/1.bfd23edd.chunk.js"
+  },
+  {
+    "revision": "18045737f644871d04ef",
+    "url": "/static/css/main.b3f308de.chunk.css"
+  },
+  {
+    "revision": "97e2fb8f62cd85558dc6b7c1fbffdd58",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d68320e1954e0d9a4481e99f7b6dcf8.js"
+  "/precache-manifest.d9ddde8e1f40947b636533b054a91b3f.js"
 );
 
 workbox.clientsClaim();
